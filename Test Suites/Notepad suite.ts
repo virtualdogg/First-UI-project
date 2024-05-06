aa0ed693f7b8e14bd167bd3ab73d7942a4a65f72
arawat <arawat@PQA-0532LT.pqatesting.com>
2024-05-06 12:40:35 -0400

    fsd

---
+++ Test Suites/Notepad suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Notepad suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fce3b58d-499d-4d16-8a5f-27b4761d7448</testSuiteGuid>
+   <testCaseLink>
+      <guid>d82fbc96-150f-4d4d-8bb7-1416f8d7a272</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Notepad2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
